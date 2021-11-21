3a5d105d3bf3b5616449053f201a8232b7638d1c
yuvraj202 <78161417+yuvraj202@users.noreply.github.com>
2021-11-21 18:25:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-function setup()
- {
-    canvas = createCanvas(380, 380);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.size(380,380);
-    video.hide();
-}
+yuvra="";
 
-
-function modelLoaded()
-{
- 
-}
-
-function draw() 
-{
-  image(video,0,50,0,0)
-}
 
 function preload()
 {
   
 }
+
+function setup()
+ {
+    canvas = createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+   
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+  document.getElementById("object_status").innerHTML="Status : Detecting Objects";
+  document.getElementById("object_name_input").value;
+}
+
+function modelLoaded()
+{
+  console.log("model Loaded");
+  yuvraj = true;
+}
+
+
+
+function draw() 
+{
+  image(video,100,180,200,200)
+}
+
+
+
